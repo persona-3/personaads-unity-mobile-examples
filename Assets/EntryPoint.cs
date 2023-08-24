d7d865baa854e5e193f55ef6d7e834512a115c33
Harshit Kumar <harshitkumar@Harshits-MacBook-Pro.local>
2023-08-25 02:48:24 +0530

    walletaddress and user email

--- Assets/EntryPoint.cs
+++ Assets/EntryPoint.cs
@@ -9,12 +9,15 @@ public class EntryPoint : MonoBehaviour
     {
         PersonaAdSDK.Initialize();
         BannerView bannerView = new BannerView(this, "9e643cf1-17f3-4de8-b28d-eb85eef60f91", 0, 100);
+
+        bannerView.SetWalletAddress("0xYourWalletAddress"); // Example wallet address - Optional field
+        bannerView.SetUserEmail("sample.email@example.com"); // Example user email - Optional field
         bannerView.LoadAd();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
